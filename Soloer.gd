94f3f96c660a2b6fa82da51a88d36d99a4093dd5
JaumeGreen <greennight@gmail.com>
2024-07-23 18:33:30 +0200

    Personatge es mou amb cursos i ratolí
    Ueeee

--- Soloer.gd
+++ Soloer.gd
@@ -6,33 +6,43 @@ var target = position
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
+	if input_direction :
+		velocity = input_direction * speed
+		target = position
+	return input_direction
 
 func _input(event):
 	if event.is_action_pressed("click"):
 		target = get_global_mouse_position()
 
+func look_direction(dir):
+	if dir.x < -0.4 && dir.y < -0.4 :
+		_personatge.play("NW")
+	if dir.x < -0.4 && dir.y > 0.4 :
+		_personatge.play("SW")
+	if dir.x < -0.4 && dir.y >-0.4 && dir.y < 0.4 :
+		_personatge.play("W")
+	if dir.x > 0.4 && dir.y < -0.4 :
+		_personatge.play("NE")
+	if dir.x > 0.4 && dir.y > 0.4 :
+		_personatge.play("SE")
+	if dir.x > 0.4 && dir.y >-0.4 && dir.y < 0.4 :
+		_personatge.play("E")
+	if dir.x < 0.4 && dir.x > -0.4 && dir.y < -0.4 :
+		_personatge.play("N")
+	if dir.x < 0.4 && dir.x > -0.4 && dir.y > 0.4 :
+		_personatge.play("S")
+
+
 func _physics_process(_delta):
-	#get_input()
 	var dir = position.direction_to(target)
 	velocity = dir * speed
-	if position.distance_to(target) > 10:
-		if dir.x < -0.4 && dir.y < -0.4 :
-			_personatge.play("NW")
-		if dir.x < -0.4 && dir.y > 0.4 :
-			_personatge.play("SW")
-		if dir.x < -0.4 && dir.y >-0.4 && dir.y < 0.4 :
-			_personatge.play("W")
-		if dir.x > 0.4 && dir.y < -0.4 :
-			_personatge.play("NE")
-		if dir.x > 0.4 && dir.y > 0.4 :
-			_personatge.play("SE")
-		if dir.x > 0.4 && dir.y >-0.4 && dir.y < 0.4 :
-			_personatge.play("E")
-		if dir.x < 0.4 && dir.x > -0.4 && dir.y < -0.4 :
-			_personatge.play("N")
-		if dir.x < 0.4 && dir.x > -0.4 && dir.y > 0.4 :
-			_personatge.play("S")
+	var dir2 = get_input()
+	if position.distance_to(target) > 10 :
+		look_direction(dir)
+		move_and_slide()
+	elif dir2 :
+		look_direction(dir2)
 		move_and_slide()
 	else :
 		_personatge.play("SStill")
